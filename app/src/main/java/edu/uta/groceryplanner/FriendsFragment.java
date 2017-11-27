9e747500f148cd8626c6255bbc1bde87a1dd5672
Vaibhav Naik <vaibhavsnaik09@gmail.com>
2017-11-26 20:41:52 -0600

    Friends add : invite US. Warnings and restrictions added on the input

--- app/src/main/java/edu/uta/groceryplanner/FriendsFragment.java
+++ app/src/main/java/edu/uta/groceryplanner/FriendsFragment.java
@@ -11,13 +11,17 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -36,45 +40,46 @@ import edu.uta.groceryplanner.beans.UserBean;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FriendsFragment extends Fragment implements View.OnClickListener{
+public class FriendsFragment extends Fragment implements View.OnClickListener {
     private FirebaseAuth firebaseAuth;
-    private FloatingActionButton mainFab,personalFab,billFab;
-    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
+    private FloatingActionButton mainFab, personalFab, billFab;
+    private Animation fab_open, fab_close, rotate_forward, rotate_backward;
     private Boolean isFabOpen = false;
-    DatabaseReference friendsRef,usersRef;
+    DatabaseReference friendsRef, usersRef;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List<FriendsBean> friends;
-
+    private static final int REQUEST_INVITE = 0;
     public FriendsFragment() {
         // Required empty public constructor
     }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        firebaseAuth=FirebaseAuth.getInstance();
-        if(firebaseAuth==null){
-            startActivity(new Intent(getContext(),LoginActivity.class));
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth == null) {
+            startActivity(new Intent(getContext(), LoginActivity.class));
         }
         friendsRef = FirebaseDatabase.getInstance().getReference("friends").child(firebaseAuth.getCurrentUser().getUid());
-        usersRef= FirebaseDatabase.getInstance().getReference("users");
+        usersRef = FirebaseDatabase.getInstance().getReference("users");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=  inflater.inflate(R.layout.fragment_friends, container, false);
-        recyclerView=view.findViewById(R.id.friendsRecyclerView);
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        recyclerView = view.findViewById(R.id.friendsRecyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        friends=new ArrayList<>();
+        friends = new ArrayList<>();
         mainFab = view.findViewById(R.id.mainFab);
         personalFab = view.findViewById(R.id.personalFab);
         billFab = view.findViewById(R.id.billFab);
         fab_open = AnimationUtils.loadAnimation(getContext(), R.anim.fab_open);
-        fab_close = AnimationUtils.loadAnimation(getContext(),R.anim.fab_close);
-        rotate_forward = AnimationUtils.loadAnimation(getContext(),R.anim.rotate_forward);
-        rotate_backward = AnimationUtils.loadAnimation(getContext(),R.anim.rotate_backward);
+        fab_close = AnimationUtils.loadAnimation(getContext(), R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getContext(), R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getContext(), R.anim.rotate_backward);
         mainFab.setOnClickListener(this);
         personalFab.setOnClickListener(this);
         billFab.setOnClickListener(this);
@@ -85,7 +90,7 @@ public class FriendsFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        switch (id){
+        switch (id) {
             case R.id.mainFab:
                 animateFAB();
                 break;
@@ -93,54 +98,91 @@ public class FriendsFragment extends Fragment implements View.OnClickListener{
                 animateFAB();
                 LayoutInflater li = LayoutInflater.from(getContext());
                 View modal = li.inflate(R.layout.friend_modal, null);
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
-                alertDialogBuilder.setView(modal);
-                final EditText textFriendEmail= modal.findViewById(R.id.TextFriendEmail);
-                final String friendEmail=textFriendEmail.getText().toString();
-                alertDialogBuilder.setCancelable(false)
-                        .setPositiveButton("Add",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        /*if(TextUtils.isEmpty(friendEmail)){
-                                            textFriendEmail.setError("EmailId cannot be empty",getResources().getDrawable(R.drawable.ic_warning_black_24dp));
-                                            return;
-                                        }
-                                        else if(friendEmail.equalsIgnoreCase(firebaseAuth.getCurrentUser().getEmail())){
-                                            textFriendEmail.setError("EmailId and User Email cannot be same",getResources().getDrawable(R.drawable.ic_warning_black_24dp));
-                                            return;
-                                        }*/
-                                        usersRef.orderByChild("emailId").equalTo(textFriendEmail.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
-                                            @Override
-                                            public void onDataChange(DataSnapshot snapshot) {
-                                                if (snapshot.exists()) {
-                                                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                                                        UserBean userBean=dataSnapshot.getValue(UserBean.class);
-                                                        friendsRef.child(userBean.getUserId()).setValue(new FriendsBean(userBean.getUserId(),userBean.getUserName(),userBean.getEmailId(),"resolved",0));
-                                                    }
-                                                }else{
-                                                }
-                                            }
-                                            @Override
-                                            public void onCancelled(DatabaseError databaseError) {
-
-                                            }
-                                        });
-                                    }
-                                })
+                final EditText textFriendEmail = modal.findViewById(R.id.TextFriendEmail);
+                final Button btnFriendInvite=modal.findViewById(R.id.btnFriendInvite);
+                AlertDialog dialog = new AlertDialog.Builder(getContext())
+                        .setView(modal)
+                        .setCancelable(false)
+                        .setPositiveButton("Add", null)
                         .setNegativeButton("Cancel",
                                 new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,int id) {
+                                    public void onClick(DialogInterface dialog, int id) {
                                         animateFAB();
                                         dialog.cancel();
                                     }
-                                });
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
+                                }).create();
+                dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(final DialogInterface dialog) {
+                        Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                        button.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View view) {
+                                String friendEmail = textFriendEmail.getText().toString();
+                                Log.v("friend",friendEmail);
+                                if (TextUtils.isEmpty(friendEmail)) {
+                                    textFriendEmail.setError("EmailId cannot be empty", getResources().getDrawable(R.drawable.ic_warning_black_24dp));
+                                }
+                                else if(!friendEmail.contains("@")){
+                                    textFriendEmail.setError("EmailId not valid", getResources().getDrawable(R.drawable.ic_warning_black_24dp));
+                                }
+                                else if (friendEmail.equalsIgnoreCase(firebaseAuth.getCurrentUser().getEmail())) {
+                                    textFriendEmail.setError("EmailId and User Email cannot be same", getResources().getDrawable(R.drawable.ic_warning_black_24dp));
+                                }
+                                else {
+                                    usersRef.orderByChild("emailId").equalTo(textFriendEmail.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot snapshot) {
+                                            if (snapshot.exists()) {
+                                                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                                    UserBean userBean = dataSnapshot.getValue(UserBean.class);
+                                                    if("active".equalsIgnoreCase(userBean.getStatus())) {
+                                                        friendsRef.child(userBean.getUserId()).setValue(new FriendsBean(userBean.getUserId(), userBean.getUserName(), userBean.getEmailId(), "resolved", 0));
+                                                        dialog.dismiss();
+                                                    }
+                                                    else
+                                                        textFriendEmail.setError("your friend is inactive, Contact Admin", getResources().getDrawable(R.drawable.ic_warning_black_24dp));
+                                                }
+                                            } else {
+                                                textFriendEmail.setError("No Email Id found, Do you want to invite?", getResources().getDrawable(R.drawable.ic_warning_black_24dp));
+                                                btnFriendInvite.setVisibility(View.VISIBLE);
+                                                btnFriendInvite.setOnClickListener(new View.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(View view) {
+                                                        dialog.dismiss();
+                                                        btnFriendInvite.setVisibility(View.GONE);
+                                                        animateFAB();
+                                                        sendInvite();
+                                                    }
+                                                });
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+                                            dialog.dismiss();
+                                            animateFAB();
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                    }
+                });
+                dialog.show();
                 break;
             case R.id.billFab:
                 break;
         }
     }
+    public void sendInvite(){
+        Intent intent = new AppInviteInvitation.IntentBuilder(getString(R.string.invitation_title))
+                .setMessage(getString(R.string.invitation_message))
+                .setEmailHtmlContent(getString(R.string.invitation_html_content))
+                .setEmailSubject(getString(R.string.invitation_email_subject))
+                .build();
+        startActivityForResult(intent, REQUEST_INVITE);
+    }
+
     public void animateFAB() {
 
         if (isFabOpen) {
@@ -159,20 +201,22 @@ public class FriendsFragment extends Fragment implements View.OnClickListener{
             isFabOpen = true;
         }
     }
+
     @Override
     public void onStart() {
         friendsRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 friends.clear();
-                for (DataSnapshot dataSnap : dataSnapshot.getChildren()){
-                    FriendsBean friendsBean=dataSnap.getValue(FriendsBean.class);
+                for (DataSnapshot dataSnap : dataSnapshot.getChildren()) {
+                    FriendsBean friendsBean = dataSnap.getValue(FriendsBean.class);
                     friends.add(friendsBean);
                 }
-                adapter=new FriendsAdapter(friends,getContext());
+                adapter = new FriendsAdapter(friends, getContext());
                 recyclerView.setItemAnimator(new DefaultItemAnimator());
                 recyclerView.setAdapter(adapter);
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
