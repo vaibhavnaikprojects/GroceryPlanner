aab4dab02dcd6e65b9f0c06792f86073e3c33f5e
Prakhar Sapre <prakhar.sapre2610@gmail.com>
2017-11-26 18:34:00 -0600

    Code fix for app invite

--- app/src/main/java/edu/uta/groceryplanner/InviteActivity.java
+++ app/src/main/java/edu/uta/groceryplanner/InviteActivity.java
@@ -15,67 +15,17 @@ import android.widget.Toast;
 import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class InviteActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private TextView editTextEmail;
-    private Button btnSendInvite;
-    private FirebaseAuth firebaseAuth;
-    private static final int REQUEST_INVITE = 0;
+public class InviteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite);
-
-        firebaseAuth=FirebaseAuth.getInstance();
-        if(firebaseAuth==null){
-            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-        }
-
-        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-        btnSendInvite = findViewById(R.id.btnSendInvite);
-
-        btnSendInvite.setOnClickListener(this);
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
         Intent homeIntent = new Intent(getApplicationContext(), HomeActivity.class);
         startActivityForResult(homeIntent, 0);
         return true;
-
-    }
-
-    @Override
-    public void onClick(View view) {
-        if(view == btnSendInvite){
-            sendInvite();
-        }
-    }
-
-    public void sendInvite(){
-        Intent intent = new AppInviteInvitation.IntentBuilder(getString(R.string.invitation_title))
-                .setMessage(getString(R.string.invitation_message))
-                .setEmailHtmlContent(getString(R.string.invitation_html_content))
-                .setEmailSubject(getString(R.string.invitation_email_subject))
-                .build();
-        startActivityForResult(intent, REQUEST_INVITE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Log.d("TAG", "onActivityResult: requestCode=" + requestCode + ", resultCode=" + resultCode);
-
-        if (requestCode == REQUEST_INVITE) {
-            if (resultCode == RESULT_OK) {
-                // Get the invitation IDs of all sent messages
-                String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
-                for (String id : ids) {
-                    Log.d("TAG", "onActivityResult: sent invitation " + id);
-                }
-            } else {
-                Toast.makeText(getApplicationContext(),"Failed to send invite",Toast.LENGTH_LONG).show();
-            }
-        }
     }
 }
