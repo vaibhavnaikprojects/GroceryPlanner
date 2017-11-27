0c978a980193970391e85b2e066fb8ded08fce9f
nikhilkumar kekan <nikhil19kekan@gmail.com>
2017-11-26 20:44:47 -0600

    activity name corrected

--- app/src/main/java/edu/uta/groceryplanner/BarcodeMain.java
+++ app/src/main/java/edu/uta/groceryplanner/BarcodeMain.java
@@ -44,7 +44,7 @@ public class BarcodeMain extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.barcode_activity_main);
 
         statusMessage = (TextView)findViewById(R.id.status_message);
         barcodeValue = (TextView)findViewById(R.id.barcode_value);
